e464fb807c170e4cd35f87b6dc6afdbb38af0e7c
Shellowb <43820167+Shellowb@users.noreply.github.com>
2019-03-08 10:51:30 -0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,28 +1,28 @@
+"""
+Main settings and functions to work from the server to Arduino Devices.
+By: Marcelo Becerra A.
+marcelo.becerra@ug.uchile.cl
+"""
 import serial
 import io
 import time
-# Water, kitchen
+
+# Water, kitchen Arduino Port
 ser = serial.Serial("/dev/ttyUSB0",9600, timeout= 0.5)
-# powder
+
+# powder Arduino Port
 ser1 = serial.Serial("/dev/ttyACM1",9600, timeout= 0.5)
+
+
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 #time.sleep(3)
+
+# Testing Arduino led
 def check_led():
     ser.write(b'c\n')
     ser.flush()
-    """
-    a = str(ser.readline())
-    print("a =", a)
-    ser.write(b'2')
-    a = str(ser.readline())
-    print("a =", a)
-    ser.write(b'0')
-    ser.write(b'0')
-    ser.write(b'e')
-    ser.write(b'c')
-    a = str(ser.readline())
-    print("a =", a)"""
 
+# Turn on the kitchen
 def turn_on_pot():
     ser.write(b'be\n')
     ser.flush()
@@ -30,6 +30,7 @@ def turn_on_pot():
     #print(ser.readline())
     ser.flush()
 
+# Show information from serial port
 def show():
     ser.write(b'c\n')
     a= str(ser.readline())
@@ -38,6 +39,7 @@ def show():
     print(a)
     return c
 
+# Send Proportions of materials to the machine
 def send_proportions(agar,water,glyc):
     start='a'
     end = 'e'
